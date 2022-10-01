e41736a15e91c11bb8dee5d207d19d6e84179e17
Sabarno Chatterjee <106575210+Sabarno-Chatterjee@users.noreply.github.com>
2022-10-01 10:09:19 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ email = []
 company = []
 membership_status = []
 
+is_continue = 1
 
-name.append(input("Name: "))
-contact.append(input("Contact Number: "))
-postal_address.append(input("Address: "))
-email.append(input("Email: "))
-company.append(input("Company: "))
-membership_status.append("Membership status: ")
+while is_continue:
+    name.append(input("Name: "))
+    contact.append(input("Contact Number: "))
+    postal_address.append(input("Address: "))
+    email.append(input("Email: "))
+    company.append(input("Company: "))
+    membership_status.append(input("Membership status: "))
+
+    if (input("Do you want to enter more data, Type 'Y' or 'N'").lower()) == "n":
+        is_continue = 0
+
+print(name, contact, postal_address, email, company, membership_status)
